f5fa7db82ecd10d6a7c6d9e6f52d1e69b2f90b94
Thiruna <thirunavukkarasu@skematix.in>
2025-11-14 20:44:14 +0530

    Blogs - initial commit

--- src/components/home/NavBarComponent.tsx
+++ src/components/home/NavBarComponent.tsx
@@ -69,7 +69,7 @@ function NavBarComponent() {
           <ul className="font-mono font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -78,7 +78,7 @@ function NavBarComponent() {
             </li>
             <li>
               <a
-                href="#"
+                href="/projects"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
                 Projects
@@ -86,7 +86,7 @@ function NavBarComponent() {
             </li>
             <li>
               <a
-                href="#"
+                href="blogs"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
                 Blogs
@@ -94,7 +94,7 @@ function NavBarComponent() {
             </li>
             <li>
               <a
-                href="#"
+                href="contact"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
                 Contact
@@ -102,7 +102,7 @@ function NavBarComponent() {
             </li>
             <li>
               <a
-                href="#"
+                href="my-resume"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
                 My Resume
